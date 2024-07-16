02061869e86482513662c48f0ad549070e6e166f
Sofiia13 <sofiia.yevush@gmail.com>
2024-07-16 23:15:06 +0300

    Add styles for responive design

--- frontend/src/Components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -40,7 +40,7 @@ const ProductDisplay = (props) => {
                 </div>
                 <div className="productdisplay-right-size">
                     <h1>Select Size</h1>
-                    <div className="productdisplay-right-size">
+                    <div className="productdisplay-right-sizes">
                         <div>S</div>
                         <div>M</div>
                         <div>L</div>
