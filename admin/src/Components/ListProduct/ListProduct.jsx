016451b3f8c9aa45b8b66720d21a74b7eb78d479
Sofiia13 <sofiia.yevush@gmail.com>
2024-07-22 15:38:07 +0300

    Add panel for the list of products and logic to it

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -16,6 +16,18 @@ const ListProduct = () => {
         fetchInfo();
     },[])//only once
 
+    const remove_product = async (productId) => {
+        await fetch('http://localhost:4000/removeproduct', {
+            method:'POST',
+            headers:{
+                Accept:'application/json',
+                'Content-Type':'application/json',
+            },
+            body:JSON.stringify({id:productId})
+        })
+        await fetchInfo();
+    }
+
     return (
         <div className='list-product'>
             <h1>All Products List</h1>
@@ -30,14 +42,16 @@ const ListProduct = () => {
             <div className="listproduct-allproducts">
                 <hr />
                 {allproducts.map((product,index)=>{
-                    return <div key={index} className="listproduct-format-main listproduct-format">
+                    return <><div key={index} className="listproduct-format-main listproduct-format">
                         <img src={product.image} alt="" className="listproduct-product-icon" />
                         <p>{product.name}</p>
                         <p>${product.old_price}</p>
                         <p>${product.new_price}</p>
                         <p>{product.category}</p>
-                        <img src={cross_icon} alt="" className="listproduct-remove-icon" />
+                        <img onClick={()=>{remove_product(product.id)}} src={cross_icon} alt="" className="listproduct-remove-icon" />
                     </div>
+                    <hr />
+                    </>
                 })}
             </div>
         </div>
