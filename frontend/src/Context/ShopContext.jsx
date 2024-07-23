41cef09c46ece15e0df7df3620c866a74c5b7a7a
Sofiia13 <sofiia.yevush@gmail.com>
2024-07-23 15:28:29 +0300

    Add logic for getting data from database

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -17,7 +17,21 @@ const ShopContextProvider = (props) => {
     useEffect(()=>{
         fetch('http://localhost:4000/allproducts')
         .then((response)=>response.json())
-        .then((data)=>setAll_product(data))
+        .then((data)=>setAll_product(data));
+
+        if(localStorage.getItem('auth-token')) {
+            fetch('http://localhost:4000/getcart',{
+                method:'POST',
+                headers:{
+                    Accept:'application/form-data',
+                    'auth-token':`${localStorage.getItem('auth-token')}`,
+                    'Content-Type':'application/json',
+                },
+                body:'',
+            })
+            .then((response)=>response.json())
+            .then((data)=>setCartItems(data)); 
+        }
     },[])
         
     const addToCart = (itemId) => {
